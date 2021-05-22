78ac1dcca402ab19dd0ad1733bebfaff7c203170
Maroussia Arnault <maroussia.arnault@gmail.com>
2021-05-22 19:28:28 +0200

    added event emitter and listener for the Comment Created and Updated

---
+++ comments/README.md
@@ -0,0 +1,3 @@
+# Comment Service
+
+In charge of all the business logic around a comment.
--- comments/index.js
+++ comments/index.js
@@ -24,27 +24,58 @@ app.post('/posts/:id/comments', async (req, res) => {
     // check if there is a comment associated to a post if that results as undefinied then give an empty array
     const comments = commentsByPostId[req.params.id] || [];
 
-    comments.push({ id: commentId, content });
+    // create the comment itself
+    comments.push({ id: commentId, content, status: 'pending' });
 
     commentsByPostId[req.params.id] = comments;
 
-    // Listen to the broker
+    // Listen to the broker. Create the CommentCreated Event
     await axios.post('http://localhost:4005/events', {
         type: 'CommentCreated',
         data: {
             id: commentId,
             content,
-            postId: req.params.id
+            postId: req.params.id,
+            status: 'pending'
         }
-    });
+    }).catch((err) => {
+        console.log(err.message);
+      });
 
     // send the response to the user
     res.status(201).send(comments);
+
 });
 
 // listen to the event
-app.post('/events', (req, res) => {
+app.post('/events', async (req, res) => {
     console.log('Event Received', req.body.type);
+
+    const { type, data } = req.body;
+
+    if (type === 'CommentCreated') {
+        const { postId, id, status, content } = data;
+
+        const comments = commentsByPostId[postId];
+
+        const comment = comments.find(comment => {
+            return comment.id === id;
+        });
+
+        comment.status = status;
+
+        // tell every services there is an update: emit CommentUpdated
+        await axios.post('http://localhost:4005/events', {
+            type: 'CommentUpdated',
+            data: {
+                id,
+                status,
+                postId,
+                content
+            }
+        });
+    };
+
 
     res.send({});
 });
